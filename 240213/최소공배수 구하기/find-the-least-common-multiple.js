cbfdc282accaacaca955930c6d2cedf80effbd94
이상희 <sang6299@naver.com>
2024-02-13 18:06:03 +0900

    Title: 최소공배수 구하기, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240213/최소공배수 구하기/find-the-least-common-multiple.js
@@ -0,0 +1,12 @@
+function findLCM (n, m){
+    let lcm = 1;
+    for(let i =1; i <= Math.min(n,m); i++){
+        if(n%i === 0 && m%i === 0) lcm *=i;
+    }
+    console.log(lcm);
+}
+
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+const [n, m] = input.map(Number);
+findLCM(n, m);
