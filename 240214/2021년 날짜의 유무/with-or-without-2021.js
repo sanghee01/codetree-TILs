b7f8707d9bc71f1f523d0c9102fe07289e1711f2
이상희 <sang6299@naver.com>
2024-02-14 16:35:28 +0900

    Title: 2021년 날짜의 유무, Time: 250ms, Memory: 14MB, Status: Passed - CodeTree

--- 240214/2021년 날짜의 유무/with-or-without-2021.js
+++ 240214/2021년 날짜의 유무/with-or-without-2021.js
@@ -1,11 +1,16 @@
 function isExist(m, d){
-    let answer = "No";
     const date = [[1,31],[2,28],[3,31],[4,30],[5,31],[6,30],[7,31],[8,31],[9,30],[10,31],[11,30],[12,31]];
-    if(date[m-1][1] >= d) answer="Yes";
+    if(m>12) return false
+    if(date[m-1][1] >= d) return true;
     
-    return answer;
+    return false;
 }
 
 const fs = require("fs");
 const [m, d] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
-console.log(isExist(m, d));
+
+if(isExist(m,d)){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
