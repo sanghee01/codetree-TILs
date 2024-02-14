e75aff6263475fcd791d534ec9692cde24cdb883
이상희 <sang6299@naver.com>
2024-02-14 16:33:04 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240214/2021년 날짜의 유무/with-or-without-2021.js
+++ 240214/2021년 날짜의 유무/with-or-without-2021.js
@@ -1,5 +1,5 @@
 function isExist(m, d){
-    let answer = "No"
+    let answer = "No";
     const date = [[1,31],[2,28],[3,31],[4,30],[5,31],[6,30],[7,31],[8,31],[9,30],[10,31],[11,30],[12,31]];
     if(date[m-1][1] >= d) answer="Yes";
     
