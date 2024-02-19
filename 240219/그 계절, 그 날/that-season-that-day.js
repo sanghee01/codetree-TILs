2461f3b9ceeed6fef33cba49e24a9699cc41e1dd
이상희 <sang6299@naver.com>
2024-02-19 18:41:19 +0900

    Title: 그 계절, 그 날, Time: 238ms, Memory: 47MB, Status: Passed - CodeTree

--- 240219/그 계절, 그 날/that-season-that-day.js
+++ 240219/그 계절, 그 날/that-season-that-day.js
@@ -6,7 +6,7 @@ function isYunNyun(y){
 }
 
 function lastDayIs(y,m,d){
-    if (m === 1 && m === 3 && m === 5 && m === 7 && m === 8 && m === 10 && m === 12){
+    if (m === 1 || m === 3 || m === 5 || m === 7 || m === 8 || m === 10 || m === 12){
         if (d <= 31) return true;
     } else if(m === 2 && isYunNyun(y)){
         if (d <= 29) return true;
