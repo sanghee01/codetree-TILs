ef0c38791a73c5a482c189889f2cfc10579c9e29
이상희 <sang6299@naver.com>
2024-02-22 16:01:45 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 240222/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
+++ 240222/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 const n = Number(input[0]);
 const numList = input[1].split(" ").map(Number);
-let max;
+let max = 0;
 
 function maxNum(n){
     if(n<0) return max;
     
-    if(numList[n-1] > numList[n]){ 
-        max = numList[n-1];
+    if(numList[n] > max){ 
+        max = numList[n];
     }
     return maxNum(n-1);
 }
