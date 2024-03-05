5740682ae3e0a920561e8f8cf0c0dc1d80163132
이상희 <sang6299@naver.com>
2024-03-05 23:37:47 +0900

    Title: 100으로 나눈 나머지의 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240305/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.js
+++ 240305/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let number = Number(fs.readFileSync(0).toString().trim());
 
 function getNum(a, b){
-    number--;
+    number -= 1;
     if(number === 1) return b;
     return getNum(b, (a*b)%100);        
 }
