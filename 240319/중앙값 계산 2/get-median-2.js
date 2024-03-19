146ba229b40feb7f9f1c89a242645b97ccd46174
이상희 <sang6299@naver.com>
2024-03-19 16:53:18 +0900

    Title: 중앙값 계산 2, Time: 205ms, Memory: 14MB, Status: Passed - CodeTree

--- 240319/중앙값 계산 2/get-median-2.js
+++ 240319/중앙값 계산 2/get-median-2.js
@@ -5,8 +5,9 @@ const list = input[1].split(" ").map(Number);
 function middleNum(list){
     const currentList = [];
     for(let i = 1; i<=n;i++){
+        let sortList; 
         currentList.push(list[i-1]);
-
+        sortList = currentList.sort((a,b)=>{return a-b})
         if(i % 2 === 1){
             process.stdout.write(currentList[Math.floor(currentList.length/2)] + " ");
         }
