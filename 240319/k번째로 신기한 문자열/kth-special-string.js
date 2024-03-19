170b530abde70aa5fa079de4d238489379301df8
이상희 <sang6299@naver.com>
2024-03-19 17:09:09 +0900

    Title: k번째로 신기한 문자열, Time: 156ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240319/k번째로 신기한 문자열/kth-special-string.js
@@ -0,0 +1,22 @@
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+const [n, k, t] = input[0].split(" ");
+input.shift();
+let list = [];
+
+function returnStartT(input){
+    for(let x of input){
+        if(x.includes(t)) list.push(x);
+    }
+    list.sort();
+
+    return list;
+}
+
+function printIndexK(input,k){
+    returnStartT(input);
+    
+    return list[k-1]
+
+}
+
+console.log(printIndexK(input, k));
