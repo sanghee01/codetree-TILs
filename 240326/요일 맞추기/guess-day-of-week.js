f29276dd67e87934d8dbcc61569372f5d54b9480
이상희 <sang6299@naver.com>
2024-03-26 16:22:33 +0900

    Title: 요일 맞추기, Time: 218ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240326/요일 맞추기/guess-day-of-week.js
@@ -0,0 +1,31 @@
+const [m1,d1,m2,d2] = require("fs").readFileSync(0).toString().trim().split(" ").map(Number);
+
+const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const week = ["Mon", "Tue","Wed","Thu","Fri","Sat","Sun"];
+let answer;
+
+function getDay(m,d){
+    let day = 0;
+    for(let i = 1; i<m;i++){
+        day += num_of_days[i];
+    }
+    day += d;
+
+    return day;
+}
+
+const interDays = getDay(m2,d2) - getDay(m1,d1);
+
+if(interDays > 0){
+    answer = week[interDays%7];
+}else{
+    answer = week[week.length - Math.abs(interDays)%7]
+}
+console.log(answer);
+
+
+// 날짜 구해서 빼고 나누기 7 하면 될듯.
+// 나누기 7해서 나머지 값에 따라 요일 결정
+
+// m2,d2 - m1,d1 -> 마이너스면 배열 뒤에서부터 검색
+// 플러스면 그대로
