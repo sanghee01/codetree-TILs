ede1a0e2eaee1ac50614763a1fa3b908215a17ef
이상희 <sang6299@naver.com>
2024-03-26 10:46:08 +0900

    Title: DateTime to DateTime, Time: 214ms, Memory: 14MB, Status: Passed - CodeTree

--- 240326/DateTime to DateTime/datetime-to-datetime.js
+++ 240326/DateTime to DateTime/datetime-to-datetime.js
@@ -1,3 +1,9 @@
 const [a,b,c] = require("fs").readFileSync(0).toString().trim().split(" ").map(Number);
 
-console.log((a*24*60 + b*60 + c)- (11*24*60 + 11*60 + 11));
+const getTime = (a*24*60 + b*60 + c)- (11*24*60 + 11*60 + 11);
+
+if(getTime >= 0){
+    console.log(getTime);
+}else{
+    console.log(-1);
+}
