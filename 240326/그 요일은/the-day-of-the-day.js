cf508c38d93c95e3d6b11fc3d48051bf819c7226
이상희 <sang6299@naver.com>
2024-03-26 16:45:08 +0900

    Title: 그 요일은, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240326/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,30 @@
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+const [m1,d1,m2,d2] = input[0].split(" ").map(Number);
+const day = input[1]
+
+const num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const week = ["Mon", "Tue","Wed","Thu","Fri","Sat","Sun"];
+const searchWeekIndex = week.indexOf(day);
+
+function getDays(m,d){
+    let days = 0;
+    for(let i =1; i<m; i++){
+        days += num_of_days[i];
+    }
+    days += d;
+
+    return days;
+}
+
+function getCount(interDays){
+    let count = 0;
+    for(let i = 0; i<=interDays; i++){
+        if(i%7 === searchWeekIndex) count ++;
+    }
+
+    return count;
+}
+
+let interDays = getDays(m2,d2)-getDays(m1,d1)+1;
+
+console.log(getCount(interDays));
