b33895ff0215c4db083c343b65e33bddf1881557
이상희 <sang6299@naver.com>
2024-03-27 10:03:17 +0900

    Title: 십진수와 이진수 2, Time: 220ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240327/십진수와 이진수 2/decimal-and-binary-number-2.js
@@ -0,0 +1,24 @@
+const binary = require("fs").readFileSync(0).toString().split("").map(Number);
+
+
+let num = 0;
+
+for(let i = 0; i <binary.length; i++){
+    num = num * 2 + binary[i];
+}
+
+num *= 17;
+
+const digits = []
+
+while(true){
+    if(num<2){
+        digits.push(num);
+        break;
+    }
+
+    digits.push(num%2);
+    num = Math.floor(num/2);
+}
+
+console.log(digits.reverse().join(""))
