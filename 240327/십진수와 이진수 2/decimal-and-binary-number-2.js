6f4f01d04c64441398d401d961e69a8568d47f07
이상희 <sang6299@naver.com>
2024-03-27 11:39:37 +0900

    Title: 십진수와 이진수 2, Time: 222ms, Memory: 14MB, Status: Passed - CodeTree

--- 240327/십진수와 이진수 2/decimal-and-binary-number-2.js
+++ 240327/십진수와 이진수 2/decimal-and-binary-number-2.js
@@ -1,4 +1,4 @@
-const binary = require("fs").readFileSync(0).toString().split("").map(Number);
+const binary = require("fs").readFileSync(0).toString().trim().split("").map(Number);
 
 
 let num = 0;
