c0b6ed6eff48b66f8955c9b9869f5660f2b967b6
이상희 <sang6299@naver.com>
2024-03-27 12:40:02 +0900

    Title: 블럭쌓는 명령2, Time: 183ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240327/블럭쌓는 명령2/block-stacking-commands2.js
@@ -0,0 +1,14 @@
+const input = require("fs").readFileSync(0).toString().trim().split("\n");
+const [n,k] = input[0].trim().split(" ").map(Number);
+input.shift();
+
+const blocks = new Array(n).fill(0)
+
+input.forEach((a) =>{
+    let [start, end] = a.trim().split(" ").map(Number);
+    for(let i = start; i <= end; i++){
+        blocks[i] += 1;
+    }
+})
+
+console.log(Math.max(...blocks))
